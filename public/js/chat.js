75d45da97fadd4e6043068d252dee1d476a84dfc
Jose Armando Ramirez Carrillo <ing.armandoramirez@outlook.com>
2025-01-08 17:46:10 -0600

    Final socket chat

--- public/js/chat.js
+++ public/js/chat.js
@@ -36,16 +36,51 @@ const conectarSocket = async() => {
     });
     socket.on('connect', () => console.log('sockets Online'));
     socket.on('disconnect', () => console.log('sockets Offline'));
-    socket.on('recibir-mensajes', () => {
-        //TODO
-    });
-    socket.on('usuarios-activos', () => {
-        //TODO
-    });
-    socket.on('mensaje-privado', () => {
-        //TODO
+    socket.on('recibir-mensajes', dibujarMensajes);
+    socket.on('usuarios-activos', dibujarUsuarios);
+    socket.on('mensaje-privado', (payload) => {
+        console.log('privado', payload);
     });
 }
+
+const dibujarUsuarios = (usuarios = []) => {
+    let usersHTML = '';
+    usuarios.forEach(({nombre, uid}) => {
+        usersHTML+=`
+            <li>
+                <p>
+                    <h5 class="text-success">${nombre}</h5>
+                    <span class="fs-6 text-muted">${uid}</span>
+                </p>
+            </li>
+        `
+    });
+    ulUsuarios.innerHTML = usersHTML;
+}
+
+const dibujarMensajes = (mensajes = []) => {
+    let mensajesHTML = '';
+    mensajes.forEach(({nombre, mensaje}) => {
+        mensajesHTML+=`
+            <li>
+                <p>
+                    <span class="text-primary">${nombre}: </span>
+                    <span>${mensaje}</span>
+                </p>
+            </li>
+        `
+    });
+    ulMensajes.innerHTML = mensajesHTML;
+}
+
+txtMensaje.addEventListener('keyup', ({keyCode}) => {
+    const mensaje = txtMensaje.value;
+    const uid = txtUid.value;
+    if(keyCode !== 13) return;
+    if(mensaje.lenght === 0) return;
+    socket.emit('enviar-mensaje', {mensaje,uid});
+    txtMensaje.value = '';
+});
 
 const main = async() => {
     await validarJWT
--- sockets/controller.js
+++ sockets/controller.js
@@ -1,12 +1,38 @@
 const { Socket } = require("socket.io");
 const { comprobarJWT } = require("../helpers/generar-jwt");
+const { ChatMensajes } = require("../models");
 
-const socketController = async(socket = new Socket()) => {
+const chatMensjaes = new ChatMensajes();
+
+const socketController = async(socket = new Socket(), io) => {
     const usuario = await comprobarJWT(socket.handshake.headers['x-token']);
     if(!usuario){
         socket.disconnect();
     }
-    console.log('Se conecto', usuario.nombre);
+    //Agregar el usuario conectado
+    chatMensjaes.conectarUsuario(usuario);
+    io.emit('usuarios-activos', chatMensjaes.usuariosArr);
+    socket.emit('recibir-mensjes', chatMensjaes.ultimos10);
+
+    //Conectarlo a una sala especial
+    socket.join(usuario.id);
+
+    //Limpiar cuando alguien se desconecta
+    socket.on('disconnect', () => {
+        chatMensjaes.desconectarUsuario(usuario.id);
+        io.emit('usuarios-activos', chatMensjaes.usuariosArr);
+    });
+
+    socket.on('enviar-mensaje', ({uid, mensaje}) => {
+        if(uid){
+            //privado
+            socket.to(uid).emit('mensaje-privado',{de: usuario.nombre, mensaje});
+        }else{
+            chatMensjaes.enviarMensaje(usuario.id,usuario.nombre, mensaje);
+            io.emit('recibir-mensajes',chatMensjaes.ultimos10);
+        }
+        
+    })
 }
 
 module.exports = {
